c20533cba74e4046bb0d676662ee71f101435eaa
jasony62 <jasony62@qq.com>
2020-05-11 20:58:12 +0800

    清理代码

--- src/json-doc/fields/file.js
+++ src/json-doc/fields/file.js
@@ -4,17 +4,6 @@ export class FieldFile extends Field {
   constructor(schema, schemaName) {
     super(schema, schemaName)
     this.type = 'file'
-    const fileList = [
-      {
-        name: 'food.jpeg',
-        url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg',
-      },
-      {
-        name: 'food2.jpeg',
-        url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg',
-      },
-    ]
-
-    this.value = Array.isArray(this.value) ? this.value : fileList
+    this.value = Array.isArray(this.value) ? this.value : []
   }
 }
